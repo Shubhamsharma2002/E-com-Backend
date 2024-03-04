038406c609595994ac4dc2c39ef2341d59afe296
Shubham Sharma <shubhamtech004@gmail.com>
2024-03-05 00:29:48 +0530

    crt repo change

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -6,6 +6,7 @@ let client;
     MongoClient.connect(url).then(clientInstance =>{
         client = clientInstance
         console.log("we are connected with the db ::)");
+        createCounter(client.db());
     }).catch(err =>{
         console.log(err);
     })
@@ -15,4 +16,11 @@ export const getDB = () =>{
       return client.db();
 }
 
+
+const createCounter = async(db) =>{
+    const existingCounter = await db.collection("counter").findOne({_id:'cartItemId'});
+    if(!existingCounter){
+        await db.collection("counter").insertOne({_id:'cartItemId', value:0});
+    }
+}
 // export default connectToMongodb;
